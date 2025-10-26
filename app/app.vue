9af6c6978474b907add9ce128613be305d342e85
M. Wira Ade Kusuma <mail@mwiraadekusuma.my.id>
2025-10-26 11:30:49 +0700

    fix: icon admin panel

--- app/app.vue
+++ app/app.vue
@@ -4,6 +4,19 @@
   </NuxtLayout>
 </template>
 
-<script setup lang="ts">
-// Biarkan bagian ini kosong
+<script setup>
+// Menggunakan useHead untuk memasukkan tag <link> ke dalam <head>
+useHead({
+  link: [
+    {
+      rel: 'stylesheet',
+      // Gunakan CDN Font Awesome
+      href: 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css',
+      // Pastikan atribut ini ada agar browser dapat memuatnya
+      crossorigin: 'anonymous', 
+      // Tambahkan integrity untuk keamanan (opsional tapi disarankan)
+      // integrity: 'sha512-xxxxxxxxx', 
+    }
+  ],
+});
 </script>
